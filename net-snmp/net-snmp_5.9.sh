0be5ed983e0c8f2e5a9074664317e42119018e71
l3fuex <96816282+l3fuex@users.noreply.github.com>
2025-10-26 11:01:13 +0100

    Update net-snmp_5.9.sh

--- net-snmp/net-snmp_5.9.sh
+++ net-snmp/net-snmp_5.9.sh
@@ -2,7 +2,7 @@
 
 # Installs net-snmp package from source with strong encryption support and without snmp-agent.
 
-apt install gcc make libperl-dev -y
+apt install gcc make libperl-dev libssl-dev -y
 wget https://sourceforge.net/projects/net-snmp/files/net-snmp/5.9.1/net-snmp-5.9.1.tar.gz
 tar -xf net-snmp-5.9.1.tar.gz
 cd net-snmp-5.9.1
